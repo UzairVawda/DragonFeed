a32d301803cbf7fe3e5dd81be96038dc1377556e
Uzair Vawda <umv23@drexel.edu>
2018-04-30 14:06:31 -0400

    UPDATE || allow users to logout, edit and delete events, and add event

--- src/AdminPanel/urls.py
+++ src/AdminPanel/urls.py
@@ -5,5 +5,17 @@ app_name = 'adminpanel'
 
 urlpatterns = [
     #admin panel - where the user can add an event, edit a previous event, make suggestions, and logout
-    url(r'^adminpanel$', views.AdminPanel, name='adminpanel'),
+    url(r'^$', views.AdminPanel, name='adminpanel'),
+    
+    #add event - where the user can add events 
+    url(r'^addevent$', views.AddEvent.as_view(), name="addevent"),
+
+    #admin panel - where the user can add an event, edit a previous event, make suggestions, and logout
+    url(r'^edits$', views.EditEvent.as_view(), name='edit-event'),
+
+    #delete event- deletes the event
+    url(r'^(?P<pk>[0-9]+)/delete/$', views.EventDelete.as_view(), name='event-delete'),
+
+    #edit event - where the user can go to update an event
+    url(r'^(?P<pk>[0-9]+)/update/$', views.AlbumUpdate.as_view(), name='event-update'),
 ]
--- src/AdminPanel/views.py
+++ src/AdminPanel/views.py
@@ -1,6 +1,42 @@
-from __future__ import unicode_literals
-from django.shortcuts import render
+from django.views import generic
+from Events.models import Article
+from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import  CreateView, UpdateView, DeleteView
 
 #adminpanel.html - admin panel where the user can edit certain features
 def AdminPanel(request):
+    if request.user.is_authenticated():
+        username = request.user.username
+        print(username)
+    else:
+        username = request.user.username
+        # return render(request, 'loginform.html', context)
+        return redirect('/accounts/login')
     return render(request, 'adminpanel.html')
+
+#articleform.html - allows the user to add an event
+class AddEvent(CreateView):
+    model = Article
+    fields = '__all__'
+    template_name = 'articleform.html'
+    fields = ['Title', 'Orginization', 'Location', 'Description', 'Start', 'End', 'Tag', 'Link', 'Flyer']
+
+class EditEvent(generic.ListView):
+    template_name = 'editEvent.html'
+    context_object_name = 'allArticles'
+
+    def get_queryset(self):
+        return Article.objects.all()
+
+class EventDelete(DeleteView):
+	model = Article
+	success_url = reverse_lazy('adminpanel:adminpanel')
+
+class AlbumUpdate(UpdateView):
+	model = Article
+	fields = '__all__'
+	template_name = 'articleform.html'
+	fields = ['Title', 'Orginization', 'Location', 'Description','Start','End','Link','Tag','Flyer']
+
