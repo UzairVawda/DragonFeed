91e28810d6fa83809b69de077fa18bab2e3b97aa
Uzair Vawda <umv23@drexel.edu>
2018-04-30 14:04:51 -0400

    UPDATE || adding crispy forms and updating base settings

--- src/DragonFeed/settings/base.py
+++ src/DragonFeed/settings/base.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Events',
+    'Accounts',
+    'AdminPanel',
     'crispy_forms',
-    'bootstrapform',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
